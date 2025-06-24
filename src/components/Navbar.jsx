3c989e9a812924692eb1c8523ec92f04893b895e
KanterAlon <kanter.alon@gmail.com>
2025-06-24 09:30:11 -0300

    Estilado y correccion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,37 +6,41 @@ function Navbar() {
 
   useEffect(() => {
     fetch('https://dummyjson.com/products/categories')
-      .then((res) => res.json())
-      .then((data) => setCategories(data))
-      .catch(() => setCategories([]))
+      .then(res => res.json())
+      .then(data => {
+        // Si tu API devuelve { categories: [...] }, haz setCategories(data.categories)
+        setCategories(data)
+      })
+      .catch(err => {
+        console.error(err)
+        setCategories([])
+      })
   }, [])
 
   return (
     <header>
       <nav>
         <ul className="nav-list">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/quienes-somos">Quienes Somos</Link>
-          </li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/quienes-somos">Quiénes Somos</Link></li>
           <li>
             Productos
             <ul className="dropdown">
-              <li>
+              {/* Enlace “Ver todos” con clave fija */}
+              <li key="all">
                 <Link to="/productos">Ver todos</Link>
               </li>
-              {categories.map((cat) => (
-                <li key={cat}>
-                  <Link to={`/productos/${cat}`}>{cat}</Link>
+              {/* Map sobre objetos: key única y renderizado de name */}
+              {categories.map(cat => (
+                <li key={cat.slug}>
+                  <Link to={`/productos/${cat.slug}`}>
+                    {cat.name}
+                  </Link>
                 </li>
               ))}
             </ul>
           </li>
-          <li>
-            <Link to="/contacto">Contacto</Link>
-          </li>
+          <li><Link to="/contacto">Contacto</Link></li>
         </ul>
       </nav>
     </header>
