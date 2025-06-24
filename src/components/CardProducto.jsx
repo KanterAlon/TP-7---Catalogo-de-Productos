54d4deb62866b1d27de318c1994ddfabaa55f548
KanterAlon <163696462+KanterAlon@users.noreply.github.com>
2025-06-24 09:02:55 -0300

    Add basic product catalog with routing

---
+++ src/components/CardProducto.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+function CardProducto({ producto }) {
+  return (
+    <div className="card-producto">
+      <img src={producto.thumbnail} alt={producto.title} />
+      <h3>{producto.title}</h3>
+      <p>${producto.price}</p>
+      <Link to={`/producto/${producto.id}`}>Ver detalle</Link>
+    </div>
+  )
+}
+
+export default CardProducto
